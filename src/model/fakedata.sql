7a11d97aae42c6bf519321ceee02b7f55ab23cba
smartboy0310 <qdustudent6@gmail.com>
2022-04-30 12:04:15 +0500

    Edit product module

--- src/model/fakedata.sql
+++ src/model/fakedata.sql
@@ -6,3 +6,13 @@ SELECT COUNT(*) as count_client from orders WHERE order_feedback = true;
 INSERT INTO model_matras (model_name) VALUES ('Model-A');
 
 INSERT INTO products (pro_name, pro_price, pro_particle, pro_format, pro_guarantee, pro_size, pro_info, pro_images, model_id) VALUES ('Ortopedik Eko matras', 1699999, 150, '200x120x40', '3 yil', '2 kishilik', 'Penatibus viverra gravida rhoncus in. At turpis morbi ante tortor a est. Habitant adipiscing ut sed pulvinar tellus, ut urna, fermentum. Porttitor senectus lorem rhoncus facilisi ac dictum varius egestas.', ARRAY ['asasasasa','adadadadadad'], 1);
+
+
+ UPDATE 
+               products 
+         SET   
+               pro_delete_at = CURRENT_TIMESTAMP,
+               pro_is_delete = true
+         WHERE 
+               pro_id = 2
+         RETURNING * ;
